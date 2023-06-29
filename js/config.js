1e19370ce095fcc1c98a8b915ec6c9ed0e698c07
joe-morrison <joseph.morrison@live.com>
2023-06-29 13:26:30 +0200

    Added Cognito user pool details to js/config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_28uEQPZl9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6i3qa3n27bu3dek15a28krlf15', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
